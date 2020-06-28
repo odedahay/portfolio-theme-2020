e32fdd65dbd3cc9c86a5edfe3c150dfa55810fa7
Ode <odedahay@yahoo.com>
2020-06-28 08:51:56 +0800

    added pop-up

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ function onClickMenu(){
    document.getElementById("menu").classList.toggle("change");
    document.getElementById("nav").classList.toggle("change");
 }
+
+
+// modal
+
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
